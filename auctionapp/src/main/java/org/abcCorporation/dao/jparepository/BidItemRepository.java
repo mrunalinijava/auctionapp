b2a040a265549036281f92bc056a6df1312f2e53
mrunalinijava <mili4off@gmail.com>
2021-02-15 16:02:46 -0500

    first commit

--- auctionapp/src/main/java/org/abcCorporation/dao/jparepository/BidItemRepository.java
+++ auctionapp/src/main/java/org/abcCorporation/dao/jparepository/BidItemRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface BidItemRepository extends JpaRepository<BidItem, BigInteger> {
 	
-	@Query("UPDATE BID_ITEM SET NOTIFICATION_MESSAGE=:notificationMessage where AUCTION_ITEM_ID=:auctionItemId AND BIDDER_NAME=:bidderName")
+	@Query("UPDATE BidItem b SET b.notificationMessage=:notificationMessage where b.auctionItemId=:auctionItemId AND b.bidderName=:bidderName")
 	public void updateBidBasedOnAuctionItemIdAndBidderName(@Param("notificationMessage") String notificationMessage, 
 			   											   @Param("auctionItemId") BigInteger auctionItemId,
 			   											   @Param("bidderName") String bidderName);
